040c68137c08d1884834e77a07a88842eb7356eb
Tomas Hrncir <77329753+TomasHrncir@users.noreply.github.com>
2022-03-22 11:36:47 +0100

    Add files via upload

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Tom on 19.02.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
